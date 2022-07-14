3bd633bd87f4a78c4ec1e11f52fe1ac504e15278
sinetmd <98116644+sinetmd@users.noreply.github.com>
2022-07-14 23:50:40 +0300

    Added headline and hero components for fron tpage

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <main-nav />
+  <!-- only one top level parent (best practice) -->
+  <div>
+    <main-nav />
+    <hero />
+  </div>
 </template>
-
 <script>
-import MainNav from "./components/MainNav.vue";
+import MainNav from "@/components/MainNav.vue";
+import Hero from "@/components/Hero.vue";
 
 export default {
   name: "App",
   components: {
     MainNav,
+    Hero,
   },
 };
 </script>
---
+++ src/components/Headline.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h1>{{ action }} for everyone</h1>
+    <h2>Find your next job at Sainet Corp.</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Headline",
+  data() {
+    return {
+      action: "Build",
+      interval: null,
+    };
+  },
+  created() {
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    // before to get removed from the DOM
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        const currentActionIndex = actions.indexOf(this.action); // 0 starts from build when it's finished
+        const nextActionIndex = (currentActionIndex + 1) % 4; // give the index without getting out of the confines of the array
+        const nextAction = actions[nextActionIndex];
+        this.action = nextAction;
+      }, 3000);
+    },
+  },
+};
+</script>
